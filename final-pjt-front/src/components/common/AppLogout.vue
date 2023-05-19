2170962ac347c5bdfad7fea58e748c3f43761a59
arctophile19 <arctophile19@gmail.com>
2023-05-19 16:21:45 +0900

    프론트엔드 회원가입 및 로그인 기능

---
+++ final-pjt-front/src/components/common/AppLogout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-link" @click="logout">로그아웃</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      this.$store.dispatch("logout").then((flag) => {
+        if (flag) {
+          alert("로그아웃 성공")
+        } else {
+          alert("로그아웃 실패");
+        }
+      });
+    },
+  },
+};
+</script>
