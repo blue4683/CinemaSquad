56581445cc9276e16b6d997e8f772ec934fbf23d
blue4683 <an4683@gmail.com>
2023-05-19 11:43:05 +0900

    upload feature/index/code

---
+++ final-pjt-back/movies/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.movie_list),
+    path('<int:movie_pk>/', views.movie_detail),
+    path('comments/', views.comment_list),
+    path('comments/<int:comment_pk>/', views.comment_detail),
+    path('<int:movie_pk>/comments/', views.create_comment),
+]
