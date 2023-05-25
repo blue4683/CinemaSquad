8ad960bf3ab1898e4719834200534d530eedb186
arctophile19 <arctophile19@gmail.com>
2023-05-25 13:29:27 +0900

    SignUp 및 Login CSS 변경

--- final-pjt-front/src/views/LogInView.vue
+++ final-pjt-front/src/views/LogInView.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <h1>LogIn Page</h1>
+    <h2>LogIn</h2>
+    <div class="container">
     <form @submit.prevent="loginUser">
-      <label for="username">username : </label>
-      <input type="text" id="username" v-model="username"><br>
-
-      <label for="password"> password : </label>
-      <input type="password" id="password" v-model="password"><br>
-
-      <input type="submit" value="logIn">
+      <div class="input-box">
+      <input type="text" id="username" v-model="username" name="username" placeholder="아이디"><br>
+      <label for="username">아이디</label>
+      </div>
+      <div class="input-box">
+      <input type="password" id="password" v-model="password" placeholder="비밀번호"><br>
+      <label for="password">비밀번호</label>
+      </div>
+      <input type="submit" value="Login">
     </form>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -40,3 +45,55 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.container{
+  width: 400px;
+}
+.input-box{
+  position:relative;
+  margin:10px 0;
+}
+.input-box > input{
+  background:transparent;
+  border:none;
+  border-bottom: solid 1px #ccc;
+  padding:20px 0px 5px 0px;
+  font-size:14pt;
+  width:100%;
+}
+input::placeholder{
+	color:transparent;
+}
+input:placeholder-shown + label{
+  color:#aaa;
+  font-size:14pt;
+  top:15px;
+}
+input:focus + label, label{
+  color:#8aa1a1;
+  font-size:10pt;
+  pointer-events: none;
+  position: absolute;
+  left:0px;
+  top:0px;
+  transition: all 0.2s ease ;
+  -webkit-transition: all 0.2s ease;
+  -moz-transition: all 0.2s ease;
+  -o-transition: all 0.2s ease;
+}
+input:focus, input:not(:placeholder-shown){
+  border-bottom: solid 1px #8aa1a1;
+  outline:none;
+}
+ input[type=submit]{
+                background-color: #8aa1a1;
+                border:none;
+                color:white;
+                border-radius: 5px;
+                width:100%;
+                height:35px;
+                font-size: 14pt;
+                margin-top:30px;
+            }
+</style>
--- final-pjt-front/src/views/SignUpView.vue
+++ final-pjt-front/src/views/SignUpView.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
-    <h1>회원가입</h1>
+    <h1>SignUp</h1>
+    <div class="container">
     <form @submit.prevent="signUp(credentials)">
-      <label for="username">username : </label>
+      <div class="input-box">
       <input type="text" id="username" v-model="credentials.username"><br>
-
-      <label for="password1"> password : </label>
+      <label for="username">아이디</label>
+      </div>
+      <div class="input-box">
       <input type="password" id="password1" v-model="credentials.password1"><br>
-
-      <label for="password2"> password confirmation : </label>
+      <label for="password1">비밀번호</label>
+      </div>
+      <div class="input-box">
       <input type="password" id="password2" v-model="credentials.password2">
-      
+      <label for="password2">비밀번호 확인</label>
+      </div>
       <input type="submit" value="SignUp">
     </form>
+    </div>
   </div>
 </template>
 
@@ -34,3 +39,55 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.container{
+  width: 400px;
+}
+.input-box{
+  position:relative;
+  margin:10px 0;
+}
+.input-box > input{
+  background:transparent;
+  border:none;
+  border-bottom: solid 1px #ccc;
+  padding:20px 0px 5px 0px;
+  font-size:14pt;
+  width:100%;
+}
+input::placeholder{
+	color:transparent;
+}
+input:placeholder-shown + label{
+  color:#aaa;
+  font-size:14pt;
+  top:15px;
+}
+input:focus + label, label{
+  color:#8aa1a1;
+  font-size:10pt;
+  pointer-events: none;
+  position: absolute;
+  left:0px;
+  top:0px;
+  transition: all 0.2s ease ;
+  -webkit-transition: all 0.2s ease;
+  -moz-transition: all 0.2s ease;
+  -o-transition: all 0.2s ease;
+}
+input:focus, input:not(:placeholder-shown){
+  border-bottom: solid 1px #8aa1a1;
+  outline:none;
+}
+ input[type=submit]{
+                background-color: #8aa1a1;
+                border:none;
+                color:white;
+                border-radius: 5px;
+                width:100%;
+                height:35px;
+                font-size: 14pt;
+                margin-top:30px;
+            }
+</style>
